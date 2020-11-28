4137a0d95328df2908c9fee3d44f06928248831b
Herick <herick@hericks-MacBook-Pro.local>
2020-11-28 15:13:02 +0700

    First Version

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import InputForm from './component/InputForm';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App Name='Herick' Address='Cemara Asri'/>
+    <InputForm></InputForm>
   </React.StrictMode>,
   document.getElementById('root')
 );
